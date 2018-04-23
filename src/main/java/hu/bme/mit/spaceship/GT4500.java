961464ba3e72cf46b5a233dbc64e885541506263
Bruder Attila <bruderattila@gmail.com>
2018-04-23 02:01:32 +0200

    torpedo1

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -79,6 +79,9 @@ public class GT4500 implements SpaceShip {
       case ALL:
         // try to fire both of the torpedo stores
         //TODO implement feature
+		
+		//alkalmi javitas:
+		firingSuccess = true;
 
         break;
     }
